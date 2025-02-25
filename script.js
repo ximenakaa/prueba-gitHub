4b5695412bd2079a0e7de45f8f139d2c7d4467f8
ximenakaa <ximenaramirez46@gmail.com>
2025-02-24 21:09:21 -0300

    Update script.js
    agregar la frase ejercicio1 , ejercicio2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Crea un (h1) con un texto inicial y un botón.
+/* Ejercicio 1 :Crea un (h1) con un texto inicial y un botón.
 Cuando el usuario haga clic en el botón, 
 el texto del (h1) debe cambiar por otro diferente*/
 
@@ -17,7 +17,7 @@ function changeText() {
 button.addEventListener('click', changeText);
 
 
-/*Crea un campo de entrada (input de tipo texto) y un (p). 
+/* Ejercicio 2:Crea un campo de entrada (input de tipo texto) y un (p). 
  Cada vez que el usuario escriba en el campo, el (p) debe mostrar 
  un mensaje si la longitud del texto es menor a 5 caracteres.*/
 const input = document.getElementById('texto2');
